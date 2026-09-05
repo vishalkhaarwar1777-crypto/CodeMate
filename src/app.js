0939c4b5f2ca26d537cca91e9612903f8d2b0418
Vishal Kharwar <vishalkhaarwar1777@gmail.com>
2026-09-05 11:06:39 +0530

    multiple routing

--- src/app.js
+++ src/app.js
@@ -2,23 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({
-    FirstName: "vishal",
-    LastName: "kharwar"
-  });
-});
-app.get("/user/:id",(req,res) => {
-    res.send(req.params);
-});
-app.get("/user",(req,res)=>{
-  console.log(req.query);
-  res.send(req.query);
-});
-
-app.use("/test", (req, res) => {
-  res.send("Helloooo");
-});
+app.use("/user",
+(req,res,next)=>{
+  console.log("handler1");
+  //res.send("responce1");
+  next();
+},
+(req,res,next)=>{
+  console.log("hanlder2");
+  res.send("response2");
+},
+);
 
 app.listen(3000, () => {
   console.log("server is successfully listening on port 3000");
