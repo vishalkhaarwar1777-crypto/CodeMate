49433a58b1399abed933e0d83a1f084df8fae657
Vishal Kharwar <vishalkhaarwar1777@gmail.com>
2026-09-16 16:51:35 +0530

    feed API and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const User= require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName gender about skill profilePhoto";
 
@@ -48,5 +49,38 @@ userRouter.get("/user/connection",userAuth,async(req,res)=>{
   }
 });
 
+userRouter.get("/feed",userAuth,async(req,res)=>{
+  try{
+    const loggedInUser=req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit=limit>50?50:limit;
+
+    const connectionRquests=await ConnectionRequest.find({
+      $or:[
+        {fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}
+      ],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed=new Set();
+    connectionRquests.forEach((req)=>{
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const users=await User.find({
+      $and:[
+         {_id:{$nin:Array.from(hideUserFromFeed)}},
+         {_id:{$ne:loggedInUser._Id}},
+      ],
+    }).select(USER_SAFE_DATA)
+       .skip((page - 1) * limit)
+      .limit(limit);
+      res.send(users);
+  }catch(err){
+    res.status(400).send("ERROR :"+err.message)
+  }
+})
 
 module.exports = userRouter;
